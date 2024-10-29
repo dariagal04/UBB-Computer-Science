67b441e919db7ef947edd032ea365c7dbab05def
Gal Daria Cristiana <150687326+dariagal04@users.noreply.github.com>
2024-10-29 21:35:47 +0200

    Add files via upload

---
+++ Year 2/3rd Semester/Databases/lab 2/MagazineCosmetice inregistrari.sql
@@ -0,0 +1,134 @@
+﻿USE MagazineCosmetice
+
+-- Pentru tabelul Produse
+INSERT INTO Produse (Denumire, Categorie, Pret, NrBucati) 
+VALUES ('Ruj', 'Cosmetice', 50.00, 100);
+INSERT INTO Produse (Denumire, Categorie, Pret, NrBucati) 
+VALUES ('Fond de ten', 'Cosmetice', 70.00, 150);
+INSERT INTO Produse (Denumire, Categorie, Pret, NrBucati) 
+VALUES ('Mascara', 'Cosmetice', 60.00, 200);
+INSERT INTO Produse (Denumire, Categorie, Pret, NrBucati) 
+VALUES ('Anticearcan', 'Cosmetice', 30.00, 400);
+
+-- Pentru tabelul Categorii
+INSERT INTO Categorii (NumeCategorie) 
+VALUES ('Machiaj');
+INSERT INTO Categorii (NumeCategorie) 
+VALUES ('Ingrijire personala');
+INSERT INTO Categorii (NumeCategorie) 
+VALUES ('Parfumuri');
+
+-- Pentru tabelul Branduri
+INSERT INTO Branduri (NumeBrand) 
+VALUES ('BrandA');
+INSERT INTO Branduri (NumeBrand) 
+VALUES ('BrandB');
+INSERT INTO Branduri (NumeBrand) 
+VALUES ('BrandC');
+
+-- Pentru tabelul Magazine
+INSERT INTO Magazine (NumeMagazin, NumarAngajati) 
+VALUES ('Magazin1', 2);
+INSERT INTO Magazine (NumeMagazin, NumarAngajati) 
+VALUES ('Magazin2', 2);
+INSERT INTO Magazine (NumeMagazin, NumarAngajati) 
+VALUES ('Magazin3', 1);
+
+-- Pentru tabelul Angajati
+INSERT INTO Angajati (NumeAngajat, Functie, MagazinID) 
+VALUES ('Angajat1', 'Casier', 1);
+INSERT INTO Angajati (NumeAngajat, Functie, MagazinID) 
+VALUES ('Angajat2', 'Manager', 2);
+INSERT INTO Angajati (NumeAngajat, Functie, MagazinID) 
+VALUES ('Angajat3', 'Vanzator', 3);
+INSERT INTO Angajati (NumeAngajat, Functie, MagazinID) 
+VALUES ('Angajat4', 'Vanzator', 1);
+INSERT INTO Angajati (NumeAngajat, Functie, MagazinID) 
+VALUES ('Angajat5', 'Vanzator', 2);
+
+-- Pentru tabelul Clienti
+INSERT INTO Clienti (NumeClient, Email, Telefon) 
+VALUES ('Client1', 'client1@yahoo.com', '1234567890');
+INSERT INTO Clienti (NumeClient, Email, Telefon) 
+VALUES ('Client2', 'client2@yahoo.com', '0987654321');
+INSERT INTO Clienti (NumeClient, Email, Telefon) 
+VALUES ('Client3', 'client3@yahoo.com', '1243568790');
+INSERT INTO Clienti (NumeClient, Email, Telefon) 
+VALUES ('Client4', 'client4@yahoo.com', '1243658790');
+
+-- Pentru tabelul Comenzi
+INSERT INTO Comenzi (ClientID, Mid, SumaTotala) 
+VALUES (1, 1, 150.00);
+INSERT INTO Comenzi (ClientID, Mid, SumaTotala) 
+VALUES (2, 2, 200.00);
+INSERT INTO Comenzi (ClientID, Mid, SumaTotala) 
+VALUES (3, 3, 250.00);
+INSERT INTO Comenzi (ClientID, Mid, SumaTotala) 
+VALUES (4, 2, 260.00);
+
+-- Pentru tabelul ArticoleComanda
+INSERT INTO ArticoleComanda (ComandaID, Pid, Cantitate, Pret) 
+VALUES (1, 1, 2, 100.00);
+INSERT INTO ArticoleComanda (ComandaID, Pid, Cantitate, Pret) 
+VALUES (2, 2, 3, 210.00);
+INSERT INTO ArticoleComanda (ComandaID, Pid, Cantitate, Pret) 
+VALUES (3, 3, 1, 60.00);
+INSERT INTO ArticoleComanda (ComandaID, Pid, Cantitate, Pret) 
+VALUES (4, 4, 5, 300.00);
+
+
+-- Pentru tabelul ProduseBranduri
+INSERT INTO ProduseBranduri (Pid, Bid) VALUES (1, 1);
+INSERT INTO ProduseBranduri (Pid, Bid) VALUES (2, 2);
+INSERT INTO ProduseBranduri (Pid, Bid) VALUES (3, 3);
+INSERT INTO ProduseBranduri (Pid, Bid) VALUES (3, 2);
+
+-- Pentru tabelul Recenzii
+INSERT INTO Recenzii ( Pid, Rating, Comentariu, DataRecenzie) 
+VALUES ( 1, 4, 'Foarte bun', '2024-01-01');
+INSERT INTO Recenzii (Pid, Rating, Comentariu, DataRecenzie) 
+VALUES ( 2, 5, 'Excelent', '2024-01-02');
+INSERT INTO Recenzii ( Pid, Rating, Comentariu, DataRecenzie) 
+VALUES ( 3, 3, 'Acceptabil', '2024-01-03');
+INSERT INTO Recenzii ( Pid, Rating, Comentariu, DataRecenzie) 
+VALUES ( 4, 5, 'Excelent', '2024-01-04');
+
+-- Pentru tabelul ProduseCategorii
+INSERT INTO ProduseCategorii (Pid, Cid) VALUES (1, 1);
+INSERT INTO ProduseCategorii (Pid, Cid) VALUES (2, 1);
+INSERT INTO ProduseCategorii (Pid, Cid) VALUES (3, 1);
+INSERT INTO ProduseCategorii (Pid, Cid) VALUES (4, 1);
+
+
+-- Pentru a vedea înregistrările din tabelul Produse
+SELECT * FROM Produse;
+
+-- Pentru a vedea înregistrările din tabelul Categorii
+SELECT * FROM Categorii;
+
+-- Pentru a vedea înregistrările din tabelul Branduri
+SELECT * FROM Branduri;
+
+-- Pentru a vedea înregistrările din tabelul Magazine
+SELECT * FROM Magazine;
+
+-- Pentru a vedea înregistrările din tabelul Angajati
+SELECT * FROM Angajati;
+
+-- Pentru a vedea înregistrările din tabelul Clienti
+SELECT * FROM Clienti;
+
+-- Pentru a vedea înregistrările din tabelul Comenzi
+SELECT * FROM Comenzi;
+
+-- Pentru a vedea înregistrările din tabelul ArticoleComanda
+SELECT * FROM ArticoleComanda;
+
+-- Pentru a vedea înregistrările din tabelul ProduseBranduri
+SELECT * FROM ProduseBranduri;
+
+-- Pentru a vedea înregistrările din tabelul Recenzii
+SELECT * FROM Recenzii;
+
+-- Pentru a vedea înregistrările din tabelul ProduseCategorii
+SELECT * FROM ProduseCategorii;
---
+++ Year 2/3rd Semester/Databases/lab 2/MagazineCosmetice interogari.sql
@@ -0,0 +1,77 @@
+﻿USE MagazineCosmetice
+
+-- 1. Selecteaza ID-ul, denumirea si pretul produselor care au pretul mai mare sau egal cu 60.
+SELECT Pid, Denumire, Pret
+FROM Produse
+WHERE Pret >= 60;
+
+-- 2. Selecteaza ID-ul, numele angajatului si functia angajatilor care lucreaza in magazinul cu ID-ul 1.
+SELECT Aid, NumeAngajat, Functie
+FROM Angajati
+WHERE MagazinID = 1
+GROUP BY Aid, NumeAngajat, Functie;
+
+-- 3. Selecteaza ID-urile distincte ale recenziilor pentru produsul cu rating 5.
+SELECT DISTINCT Pid
+FROM Recenzii
+WHERE Rating= 5;
+
+-- 4. Selecteaza denumirile produselor si numele brandurilor distincte pentru produsele care au pretul mai mic sau egal cu 60.
+SELECT DISTINCT P.Denumire, B.NumeBrand
+FROM Produse P
+JOIN ProduseBranduri PB ON PB.Pid = P.Pid
+JOIN Branduri B ON PB.Bid = B.Bid
+WHERE P.Pret <= 60.00;
+
+-- 5. Selecteaza numele magazinelor si numarul de angajati pentru magazinele care au cel putin 2 angajati.
+SELECT M.NumeMagazin, COUNT(A.Aid) as NrAngajati
+FROM Magazine M
+JOIN Angajati A ON M.Mid = A.MagazinID
+JOIN Comenzi C ON M.Mid = C.Mid
+GROUP BY M.NumeMagazin
+HAVING COUNT(A.Aid) >= 2;
+
+-- 6. Selecteaza numele clientilor si suma totala cheltuita de fiecare client care a cheltuit mai mult de 300.
+SELECT CL.NumeClient, SUM(C.SumaTotala) as TotalCheltuit
+FROM Clienti CL
+JOIN Comenzi C ON CL.ClientID = C.ClientID
+JOIN ArticoleComanda AC ON C.ComandaID = AC.ComandaID
+GROUP BY CL.NumeClient
+HAVING SUM(C.SumaTotala) > 50;
+
+-- 7. Selecteaza denumirile produselor si categoriile lor pentru produsele care au pretul mai mare de 50.
+SELECT P.Denumire, C.NumeCategorie
+FROM Produse P
+JOIN ProduseCategorii PC ON P.Pid = PC.Pid
+JOIN Categorii C ON PC.Cid = C.Cid
+WHERE P.Pret > 50.00;
+
+-- 8. Selecteaza numele clientilor, denumirile produselor si numele magazinelor unde au fost facute comenzi
+SELECT CL.NumeClient, P.Denumire, M.NumeMagazin
+FROM Clienti CL
+JOIN Comenzi C ON CL.ClientID = C.ClientID
+JOIN ArticoleComanda AC ON C.ComandaID = AC.ComandaID
+JOIN Produse P ON AC.Pid = P.Pid
+JOIN Magazine M ON C.Mid = M.Mid;
+
+
+-- 9. Selecteaza numele clientilor si denumirile produselor comandate din categoria 'Machiaj'.
+SELECT CL.NumeClient, P.Denumire
+FROM Clienti CL
+JOIN Comenzi CM ON CL.ClientID = CM.ClientID
+JOIN ArticoleComanda AC ON CM.ComandaID = AC.ComandaID
+JOIN Produse P ON AC.Pid = P.Pid
+JOIN ProduseCategorii PC ON P.Pid = PC.Pid
+JOIN Categorii C ON PC.Cid = C.Cid
+WHERE C.NumeCategorie = 'Machiaj';
+
+-- 10. Selecteaza numele magazinelor si suma totala a articolelor comandate din brandul 'BrandB'.
+SELECT M.NumeMagazin, SUM(AC.Pret * AC.Cantitate) as TotalArticoleBrand
+FROM Magazine M
+JOIN Comenzi CM ON M.Mid = CM.Mid
+JOIN ArticoleComanda AC ON CM.ComandaID = AC.ComandaID
+JOIN Produse P ON AC.Pid = P.Pid
+JOIN ProduseBranduri PB ON P.Pid = PB.Pid
+JOIN Branduri B ON PB.Bid = B.Bid
+WHERE B.NumeBrand = 'BrandB'
+GROUP BY M.NumeMagazin;
