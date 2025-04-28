39a9ea7c203f2a3e2f67fd979ad118d2162f7f84
Gal Daria Cristiana <150687326+dariagal04@users.noreply.github.com>
2025-04-28 21:38:28 +0300

    Add files via upload

---
+++ Year 2/4th Semester/Database Management Systems\SGBD/MagazinCosmeticeAppLab2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace MagazineCosmeticeAppLab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewMagazine = new System.Windows.Forms.DataGridView();
+            this.dataGridViewAngajati = new System.Windows.Forms.DataGridView();
+            this.textBoxNume = new System.Windows.Forms.TextBox();
+            this.textBoxFunctie = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.reloadButton = new System.Windows.Forms.Button();
+            this.labelNume = new System.Windows.Forms.Label();
+            this.labelFunctie = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMagazine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAngajati)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMagazine
+            // 
+            this.dataGridViewMagazine.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewMagazine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMagazine.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewMagazine.Name = "dataGridViewMagazine";
+            this.dataGridViewMagazine.RowHeadersWidth = 51;
+            this.dataGridViewMagazine.RowTemplate.Height = 24;
+            this.dataGridViewMagazine.Size = new System.Drawing.Size(266, 245);
+            this.dataGridViewMagazine.TabIndex = 0;
+            // 
+            // dataGridViewAngajati
+            // 
+            this.dataGridViewAngajati.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridViewAngajati.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAngajati.Location = new System.Drawing.Point(266, 0);
+            this.dataGridViewAngajati.Name = "dataGridViewAngajati";
+            this.dataGridViewAngajati.RowHeadersWidth = 51;
+            this.dataGridViewAngajati.RowTemplate.Height = 24;
+            this.dataGridViewAngajati.Size = new System.Drawing.Size(431, 245);
+            this.dataGridViewAngajati.TabIndex = 1;
+            // 
+            // textBoxNume
+            // 
+            this.textBoxNume.Location = new System.Drawing.Point(98, 286);
+            this.textBoxNume.Multiline = true;
+            this.textBoxNume.Name = "textBoxNume";
+            this.textBoxNume.Size = new System.Drawing.Size(156, 34);
+            this.textBoxNume.TabIndex = 2;
+            // 
+            // textBoxFunctie
+            // 
+            this.textBoxFunctie.Location = new System.Drawing.Point(410, 283);
+            this.textBoxFunctie.Multiline = true;
+            this.textBoxFunctie.Name = "textBoxFunctie";
+            this.textBoxFunctie.Size = new System.Drawing.Size(170, 34);
+            this.textBoxFunctie.TabIndex = 3;
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(98, 377);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(120, 66);
+            this.insertButton.TabIndex = 4;
+            this.insertButton.Text = "insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(266, 377);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(121, 66);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // reloadButton
+            // 
+            this.reloadButton.Location = new System.Drawing.Point(436, 377);
+            this.reloadButton.Name = "reloadButton";
+            this.reloadButton.Size = new System.Drawing.Size(118, 66);
+            this.reloadButton.TabIndex = 6;
+            this.reloadButton.Text = "reload";
+            this.reloadButton.UseVisualStyleBackColor = true;
+            // 
+            // labelNume
+            // 
+            this.labelNume.AutoSize = true;
+            this.labelNume.Location = new System.Drawing.Point(127, 267);
+            this.labelNume.Name = "labelNume";
+            this.labelNume.Size = new System.Drawing.Size(40, 16);
+            this.labelNume.TabIndex = 8;
+            this.labelNume.Text = "Argument 1 (nume/mid)";
+            // 
+            // labelFunctie
+            // 
+            this.labelFunctie.AutoSize = true;
+            this.labelFunctie.Location = new System.Drawing.Point(433, 264);
+            this.labelFunctie.Name = "labelFunctie";
+            this.labelFunctie.Size = new System.Drawing.Size(45, 16);
+            this.labelFunctie.TabIndex = 9;
+            this.labelFunctie.Text = "Argument 2 (functie/SumaTotala)";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(696, 549);
+            this.Controls.Add(this.labelFunctie);
+            this.Controls.Add(this.labelNume);
+            this.Controls.Add(this.reloadButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.textBoxFunctie);
+            this.Controls.Add(this.textBoxNume);
+            this.Controls.Add(this.dataGridViewAngajati);
+            this.Controls.Add(this.dataGridViewMagazine);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMagazine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAngajati)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        
+
+        private System.Windows.Forms.DataGridView dataGridViewMagazine;
+        private System.Windows.Forms.DataGridView dataGridViewAngajati;
+        private System.Windows.Forms.TextBox textBoxNume;
+        private System.Windows.Forms.TextBox textBoxFunctie;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button reloadButton;
+        private System.Windows.Forms.Label labelNume;
+        private System.Windows.Forms.Label labelFunctie;
+    }
+
+}
---
+++ Year 2/4th Semester/Database Management Systems\SGBD/MagazinCosmeticeAppLab2/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration; // Necesitar pentru ConfigurationManager
+
+namespace MagazineCosmeticeAppLab2
+{
+    public partial class Form1 : Form
+    {
+        private DataSet _ds = new DataSet();
+        private SqlDataAdapter masterAdapter;
+        private SqlDataAdapter detailAdapter;
+        private BindingSource bsMaster = new BindingSource();
+        private BindingSource bsDetail = new BindingSource();
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        // Citim setările din App.config
+        string masterTable = ConfigurationManager.AppSettings["MasterTable"];
+        string detailTable = ConfigurationManager.AppSettings["DetailTable"];
+        string masterPrimaryKey = ConfigurationManager.AppSettings["MasterPrimaryKey"];
+        string detailForeignKey = ConfigurationManager.AppSettings["DetailForeignKey"];
+        string masterSelectQuery = ConfigurationManager.AppSettings["MasterSelectQuery"];
+        string detailSelectQuery = ConfigurationManager.AppSettings["DetailSelectQuery"];
+        string detailInsertQuery = ConfigurationManager.AppSettings["DetailInsertQuery"];
+        string detailDeleteQuery = ConfigurationManager.AppSettings["DetailDeleteQuery"];
+        string detailFields = ConfigurationManager.AppSettings["DetailFields"];
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    // Creăm adaptori pentru tabelele citite din App.config
+                    masterAdapter = new SqlDataAdapter(masterSelectQuery, conn);
+                    detailAdapter = new SqlDataAdapter(detailSelectQuery, conn);
+
+                    // Umplem DataSet-ul
+                    masterAdapter.Fill(_ds, masterTable);
+                    detailAdapter.Fill(_ds, detailTable);
+
+                    // Creăm relația dintre tabele
+                    DataColumn pkColumn = _ds.Tables[masterTable].Columns[masterPrimaryKey];
+                    DataColumn fkColumn = _ds.Tables[detailTable].Columns[detailForeignKey];
+                    DataRelation relation = new DataRelation($"FK_{masterTable}_{detailTable}", pkColumn, fkColumn, true);
+                    _ds.Relations.Add(relation);
+
+                    bsMaster.DataSource = _ds.Tables[masterTable];
+                    dataGridViewMagazine.DataSource = bsMaster;
+
+                    bsDetail.DataSource = bsMaster;
+                    bsDetail.DataMember = $"FK_{masterTable}_{detailTable}";
+                    dataGridViewAngajati.DataSource = bsDetail;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void reloadButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                detailAdapter = new SqlDataAdapter(detailSelectQuery, conn);
+                _ds.Tables[detailTable]?.Clear();
+                detailAdapter.Fill(_ds, detailTable);
+            }
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxNume.Text) && !string.IsNullOrEmpty(textBoxFunctie.Text))
+            {
+                try
+                {
+                    // Get selected master record id
+                    int selectedMasterId = (int)dataGridViewMagazine.CurrentRow.Cells[masterPrimaryKey].Value;
+
+                    // Split fields from the configuration to dynamically add parameters
+                    string[] fields = detailFields.Split(',');
+
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+
+                        // Create command with dynamic insert query
+                        SqlCommand cmd = new SqlCommand(detailInsertQuery, conn);
+
+                        // Add parameters dynamically based on the fields provided
+                        for (int i = 0; i < fields.Length; i++)
+                        {
+                            string paramName = "@" + fields[i];
+                            string paramValue = (i == 0) ? textBoxNume.Text : textBoxFunctie.Text;
+                            cmd.Parameters.AddWithValue(paramName, paramValue);
+                        }
+                        cmd.Parameters.AddWithValue("@masterId", selectedMasterId);
+
+                        // Execute the insert command
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Inserare reușită.");
+                        textBoxNume.Clear();
+                        textBoxFunctie.Clear();
+
+                        // Reload data after insertion
+                        reloadButton_Click(sender, e);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Eroare la inserare: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Completează toate câmpurile.");
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewAngajati.CurrentRow != null)
+            {
+                try
+                {
+                    // Get the selected detail record id
+                    int selectedDetailId = (int)dataGridViewAngajati.CurrentRow.Cells[0].Value;
+
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        // Create and execute delete command
+                        SqlCommand cmd = new SqlCommand(detailDeleteQuery, conn);
+                        cmd.Parameters.AddWithValue("@id", selectedDetailId);
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Ștergere reușită.");
+
+                        // Reload data after deletion
+                        reloadButton_Click(sender, e);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Eroare la ștergere: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selectează o înregistrare pentru ștergere.");
+            }
+        }
+
+
+    }
+}
